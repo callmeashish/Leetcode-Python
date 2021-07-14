a7c79b5381b1446e9a798636b6113ff630429bd9
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-07-14 18:28:19 -0400

    Update valid_parentheses.py

--- Algorithms/valid_parentheses.py
+++ Algorithms/valid_parentheses.py
@@ -1,8 +1,46 @@
+"""
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+Example 4:
+
+Input: s = "([)]"
+Output: false
+Example 5:
+
+Input: s = "{[]}"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+"""
+
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        end_dict = {')': '(', '}': '{', ']': '['}
-        
+        end_dict = {")": "(", "}": "{", "]": "["}
+
         for char in s:
             if char in end_dict.values():
                 stack.append(char)
@@ -16,5 +54,6 @@ class Solution:
             return True
         return False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(Solution().isValid("{[]}"))
--- README.md
+++ README.md
@@ -11,3 +11,4 @@ LeetCode problem solutions using Python
 | 9  | [Palindrome Number](https://leetcode.com/problems/palindrome-number/)         | [Python](./Algorithms/palindrome_number.py)     | :zap:      |
 | 13 | [Roman to Integer](https://leetcode.com/problems/roman-to-integer/)           | [Python](./Algorithms/roman_to_integer.py)      | :zap:      |
 | 14 | [Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/) | [Python](./Algorithms/longest_common_prefix.py) | :zap:      |
+| 14 | [Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)         | [Python](./Algorithms/valid_parentheses.py)     | :zap:      |
