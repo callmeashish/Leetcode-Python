0053d6e40358a74624f0d2e86e4d7939e9659766
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-07-14 16:48:30 -0400

    Update two_sum.py

--- Algorithms/two_sum.py
+++ Algorithms/two_sum.py
@@ -1,12 +1,39 @@
-from typing import List
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+"""
+
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list, target: int) -> list:
 
         values_dict = dict()
-        
+
         for key, value in enumerate(nums):
             complement = target - value
             if complement in values_dict:
                 return [values_dict[complement], key]
             values_dict[value] = key
+
+
+if __name__ == "__main__":
+    print(Solution().twoSum([2, 7, 11, 15], 9))
