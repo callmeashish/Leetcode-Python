208071b221532f7c7fe86e72f965dd7a3a72171b
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-07-14 18:26:38 -0400

    Update longest_common_prefix.py

--- Algorithms/longest_common_prefix.py
+++ Algorithms/longest_common_prefix.py
@@ -1,15 +1,43 @@
-from typing import List
+"""
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lower-case English letters.
+"""
+
 
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
+    def longestCommonPrefix(self, strs: list) -> str:
         lcp = ""
-        if len(strs) == 1: return strs[0]
+        if len(strs) == 1:
+            return strs[0]
 
         strs = sorted(strs)
         for first, last in zip(strs[0], strs[-1]):
-            if first == last: lcp+=first
-            else: break
+            if first == last:
+                lcp += first
+            else:
+                break
         return lcp
 
-if __name__ == '__main__':
-    print(Solution().longestCommonPrefix(["flower","flow","flight"]))
+
+if __name__ == "__main__":
+    print(Solution().longestCommonPrefix(["flower", "flow", "flight"]))
