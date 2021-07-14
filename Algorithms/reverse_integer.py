bb5d8c40262df6338d610782ce538e3aa280af0c
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-07-14 16:49:48 -0400

    Update reverse_integer.py

--- Algorithms/reverse_integer.py
+++ Algorithms/reverse_integer.py
@@ -1,3 +1,34 @@
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+Example 4:
+
+Input: x = 0
+Output: 0
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+"""
+
+
 class Solution:
     def reverse(self, x: int) -> int:
         reverse = 0
@@ -6,7 +37,11 @@ class Solution:
         else:
             reverse = -int(str(abs(x))[::-1])
 
-        if(reverse < -2147483648 or reverse > 2147483647):
+        if reverse < -2147483648 or reverse > 2147483647:
             return 0
         else:
             return int(reverse)
+
+
+if __name__ == "__main__":
+    print(Solution().reverse(123))
