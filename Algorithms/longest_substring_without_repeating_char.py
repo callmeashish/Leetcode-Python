fec313ead5adbcb9e53e1a3c5ca2f9c38ae08557
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-07-15 21:16:58 -0400

    Problem #3

---
+++ Algorithms/longest_substring_without_repeating_char.py
@@ -0,0 +1,39 @@
+class LongestSub:
+    def solution1(self, s: str) -> int:
+        length = len(s)
+        if (length <= 1) or (length == 2 and s[0] != s[1]):
+            return length
+
+        else:
+            used_char = {}
+            start = 0
+            longest_sub = ""
+            max_length = 0
+            for i, c in enumerate(s):
+                if c in used_char:
+                    start = max(start, used_char[c] + 1)
+                used_char[c] = i
+                max_length = max(max_length, i - start + 1)
+
+        return max_length
+
+    def solution1(self, s: str) -> int:
+        length = len(s)
+        if (length <= 1) or (length == 2 and s[0] != s[1]):
+            return length
+        longest_sub1 = ""
+        longest_sub2 = ""
+        for char in s:
+            if char not in longest_sub1:
+                longest_sub1 += char
+            else:
+                if len(longest_sub1) > len(longest_sub2):
+                    longest_sub2 = longest_sub1
+                longest_sub1 = longest_sub1.split(char)[1] + char
+        if len(longest_sub1) > len(longest_sub2):
+            longest_sub2 = longest_sub1
+        return len(longest_sub2)
+
+
+if __name__ == "__main__":
+    print(LongestSub().lengthOfLongestSubstring("abcabcbb"))
