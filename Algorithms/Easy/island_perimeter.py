98b27fce1f83b83fba07556c2d6126b7ad30c55a
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-08-19 22:22:04 -0400

    Problem #463
    Island Perimeter

---
+++ Algorithms/Easy/island_perimeter.py
@@ -0,0 +1,62 @@
+"""
+463. Island Perimeter
+
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
+ 
+
+Example 1:
+
+
+Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+Output: 16
+Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+Example 2:
+
+Input: grid = [[1]]
+Output: 4
+
+Example 3:
+
+Input: grid = [[1,0]]
+Output: 4
+ 
+
+Constraints:
+
+row == grid.length
+col == grid[i].length
+1 <= row, col <= 100
+grid[i][j] is 0 or 1.
+"""
+
+from typing import List
+
+
+class Perimeter:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        totalPerimeter = 0
+
+        rows = len(grid)
+        cols = len(grid[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    perimeter = 4
+                    if (j - 1) >= 0 and grid[i][j - 1] == 1:
+                        perimeter -= 1
+                    if (j + 1) < cols and grid[i][j + 1] == 1:
+                        perimeter -= 1
+                    if (i + 1) < rows and grid[i + 1][j] == 1:
+                        perimeter -= 1
+                    if (i - 1) >= 0 and grid[i - 1][j] == 1:
+                        perimeter -= 1
+
+                    totalPerimeter += perimeter
+        return totalPerimeter
