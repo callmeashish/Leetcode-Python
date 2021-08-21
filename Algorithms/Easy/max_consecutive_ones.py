079eb5d031f43dec961a8a23a373732995110b19
Ashish Kumar Singh <ashishkumarsingh046@gmail.com>
2021-08-20 22:18:40 -0400

    Problem #485
    Max Consecutive Ones

---
+++ Algorithms/Easy/max_consecutive_ones.py
@@ -0,0 +1,42 @@
+"""
+485. Max Consecutive Ones
+
+Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+"""
+
+from typing import List
+
+
+class FindMaxConsecutiveOnes:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        gmax = 0
+        count = 0
+        for v in nums:
+            if v == 1:
+                count += 1
+            else:
+                gmax = max(gmax, count)
+                count = 0
+
+        gmax = max(gmax, count)
+
+        return gmax
